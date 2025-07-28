5dc2c82f1a6d54f87882cefeccda1135c88274d0
Thibault Nicolas <thibault-n@hotmail.fr>
2025-07-28 12:20:38 +0200

    Ajout de la validation des données pour les routes d'authentification avec Zod et réorganisation des fichiers de validation.

--- src/features/auth/interface/http/routes/auth.route.ts
+++ src/features/auth/interface/http/routes/auth.route.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import { getCurrentUser, refreshToken, sendOtp, signInWithApple, signInWithGoogle, verifyOtp } from "../controllers/auth.controller.js";
+import { validate } from "@shared/utils/validator.utils.js";
+import { SendOtpValidator, VerifyOtpValidator } from "../validators/auth.validator.js";
 
 const auth = new Hono();
 
 auth.post("/sign-in-with-apple", signInWithApple);
 auth.post("/sign-in-with-google", signInWithGoogle);
-auth.post("/send-otp", sendOtp);
-auth.post("/verify-otp", verifyOtp);
+auth.post("/send-otp", validate(SendOtpValidator), sendOtp);
+auth.post("/verify-otp", validate(VerifyOtpValidator), verifyOtp);
 auth.post("/refresh-token", refreshToken);
 auth.post("/current-user", getCurrentUser);
 
--- src/features/auth/interface/http/validators/auth.validator.ts
+++ src/features/auth/interface/http/validators/auth.validator.ts
@@ -9,11 +9,3 @@ export const VerifyOtpValidator = z.object({
   email: z.email("Email invalid"),
   otp: z.string().length(6, "OTP must be 6 digits"),
 });
-
-export function validate(object: z.ZodObject) {
-  return zValidator("json", object, (result, c) => {
-    if (!result.success) {
-      return c.json({ error: result.error.message }, 400);
-    }
-  });
-}
